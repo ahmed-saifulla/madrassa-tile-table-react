d3123205bd8eb7fd1897a0d03028a9a63cd2951d
Ahmed Saifulla NS <ahmedsaifulla.ns@se.com>
2023-11-27 19:43:11 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
+import React, { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [testcols, setTestcols] = useState(['Day', 'Montg', 'Year']);
+  const [cols, setCols] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [classes, setClasses] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    let SHEET_ID = '1cYBL-8HTymg53opj6Y5T2_UIIC4ELlHUCvMz4TzPF9E'
+    let SHEET_TITLE = 'Sheet1';
+    let SHEET_RANGE = 'A1:D73'
+    let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
+
+    fetch(FULL_URL)
+      .then(res => res.text())
+      .then(rep => {
+        console.log('aa');
+        let data = JSON.parse(rep.substr(47).slice(0, -2));
+
+        console.log({ data });
+
+        let colsArray = data.table.cols.map(item => item.label)
+        let rowsArray = data.table.rows.map((item, index) => {
+          return {
+            day: (index % 2 == 0) ? item?.c[0]?.v : '',
+            class: ((index % 2 == 0) ? item?.c[1]?.v : data.table.rows[index - 1]?.c[1]?.v) || 'Pre-2',
+            period1: item?.c[2]?.v,
+            period2: item?.c[3]?.v,
+          }
+        })
+        let classesArray = data.table.rows.map((item, index) => {
+          return ((index % 2 == 0) ? item?.c[1]?.v : data.table.rows[index - 1]?.c[1]?.v) || 'Pre-2'
+        })
+        setCols(colsArray)
+        setRows(rowsArray)
+        setClasses([...new Set(classesArray)])
+
+
+
+
+      }).catch((err) => {
+        console.log('errrr', err);
+      })
+  }, []); // The empty dependency array means this effect will only run once (on mount)
+
+  useEffect(() => {
+    console.log({ rows });
+    console.log({ cols });
+    console.log({ classes });
+  }, [rows, cols])
+
+  const [selectedClass, setSelectedClass] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedClass(event.target.value);
+    let filteredDataArray = rows.filter(item => item.class == event.target.value)
+    setFilteredData(filteredDataArray);
+
+  };
+
+  const findKey = (item) => {
+    switch (item) {
+      case 'Day':
+        return 'day'
+      case 'Class':
+        return 'class'
+      case 'Period 1':
+        return 'period1'
+      case 'Period 2':
+        return 'period2'
+
+      default:
+        return ''
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor="dynamicDropdown">Select an option:</label>
+        <select id="dynamicDropdown" value={selectedClass} onChange={handleChange}>
+          {classes.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <p>Selected value: {selectedClass}</p>
+      </div>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              {cols ? cols.map(item =>
+                <>
+                  <th>{item}</th>
+                </>
+              ) : null}
+            </tr>
+          </thead>
+
+          <tbody>
+            {filteredData && filteredData.map(rowItem =>
+              <tr>
+                {cols ? cols.map((item, index) =>
+                  <td>{rowItem[findKey(item)]}</td>
+                ) : null}
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
